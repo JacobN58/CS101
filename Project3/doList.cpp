bd0042eb065bdbc5be6aba11a9f69246618d4183
JacobN58 <37630045+JacobN58@users.noreply.github.com>
2018-10-30 12:10:18 -0500

    p3

---
+++ Project3/doList.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class doList {
+public:
+        dolist(float[], float[], int );
+        void out(char );
+        void out(int i, char);
+        int insert(float, float);
+        void increase_speed(int i, float s);
+        void decrease_fuel(int i, float f);
+
+private:
+        //string dmkafkl;
+};
+
+struct Node {
+	int data;
+	struct Node* next;
+	struct Node* prev; 
+};
+
+doList::doList() {
+
+}
+
+void doList::out(char ) {
+
+}
+
+void doList::out(int i, char ) {
+
+}
+
+int doList::insert(float, float) {
+
+}
+
+void doList::increase_speed(int i, float s) {
+
+}
+
+void doList::decrease_fuel(int i, float f) {
+
+}
